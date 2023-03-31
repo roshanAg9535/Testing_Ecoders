be6a5c67c9b81a173fdd913a9989b442443a5116
Roshan Jha <Roshan Jha@SMTC-R-0204>
2023-03-31 13:12:57 +0530

    All Test url are working properly

---
+++ src/testing_all_url/Openingbrowser.java
@@ -0,0 +1,29 @@
+package testing_all_url;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Openingbrowser {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://127.0.0.1:5000/home");
+		Thread.sleep(2000);
+		String titleofpage = driver.getTitle();
+		System.out.println(titleofpage);
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		String sourcecode= driver.getPageSource();
+		System.out.println(sourcecode);
+		String Url = "http://127.0.0.1:5000/home";
+		if(Url.equals(url))
+		{
+			System.out.println("Url matched test case has passed");
+		}
+		else
+		{
+			System.out.println("Url did not match,Url test case failed");
+		}
+		driver.quit();
+	}
+
+}
---
+++ src/testing_all_url/TestingEcodersContactpage.java
@@ -0,0 +1,30 @@
+package testing_all_url;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingEcodersContactpage {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://127.0.0.1:5000/contact");
+		Thread.sleep(5000);
+		String titleofpage = driver.getTitle();
+		System.out.println(titleofpage);
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		String sourcecode= driver.getPageSource();
+		System.out.println(sourcecode);
+		String Url = "http://127.0.0.1:5000/contact";
+		if(Url.equals(url))
+		{
+			System.out.println("Url matched test case has passed");
+		}
+		else
+		{
+			System.out.println("Url did not match,Url test case failed");
+		}
+		driver.quit();
+		
+	}
+
+}
