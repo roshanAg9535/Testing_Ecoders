bdd3a1e7c5918e5d1f3ab3cc4ea6c6e064bd2d7b
Roshan Jha <Roshan Jha@SMTC-R-0204>
2023-03-31 12:57:00 +0530

    Title of all the pages working properly

---
+++ src/ecoders/Openingbrowser.java
@@ -0,0 +1,29 @@
+package ecoders;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Openingbrowser {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://127.0.0.1:5000/home");
+		Thread.sleep(2000);
+		String titleofpage = driver.getTitle();
+		System.out.println(titleofpage);
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		String sourcecode= driver.getPageSource();
+		System.out.println(sourcecode);
+		String title = "Ecoders-homepage";
+		if(title.equals(titleofpage))
+		{
+			System.out.println("Title matched test case has passed");
+		}
+		else
+		{
+			System.out.println("Tile did not match,Tile test case failed");
+		}
+		driver.quit();
+	}
+
+}
---
+++ src/ecoders/TestingEcodersContactpage.java
@@ -0,0 +1,30 @@
+package ecoders;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingEcodersContactpage {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://127.0.0.1:5000/contact");
+		Thread.sleep(5000);
+		String titleofpage = driver.getTitle();
+		System.out.println(titleofpage);
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		String sourcecode= driver.getPageSource();
+		System.out.println(sourcecode);
+		String title = "Ecoders-contactpage";
+		if(title.equals(titleofpage))
+		{
+			System.out.println("Title matched test case has passed");
+		}
+		else
+		{
+			System.out.println("Tile did not match,Tile test case failed");
+		}
+		driver.quit();
+		
+	}
+
+}
